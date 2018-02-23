845c77d68216e7c6d1da3a7e51ec851ae247e4dd
lishaoying <li_shy@hisuntech.com>
2018-02-23 12:19:56 +0800

    modify webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -15,7 +15,7 @@ module.exports = {
 
     plugins: [
         new HtmlWebpackPlugin({
-            filename: 'hj/index.html',
+            filename: 'index.html',
             template: 'src/index.html'
         }),
         new CleanWebpackPlugin(['dist'])
